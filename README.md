aa89d230e5b5073c4c0b6173a9b48b2ebdd6b10e
Adrien KISSIE <fredkiss3@gmail.com>
2024-11-19 22:48:41 +0100

    many things

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# Auto-deploy to zaneops action
+# Auto-deploy to zaneops 
+
+This action prints "Hello World" or "Hello" + the name of a person to greet to the log.
+
+## Inputs
+
+### `who-to-greet`
+
+**Required** The name of the person to greet. Default `"World"`.
+
+## Outputs
+
+### `time`
+
+The time we greeted you.
+
+## Example usage
+
+```yaml
+build-push-backend:
+name: Build and Push your image
+runs-on: ubuntu-latest
+permissions:
+    packages: write
+    contents: read
+    attestations: write
+    id-token: write
+steps:
+    - name: Checkout
+    uses: actions/checkout@v4
+    - name: Set up QEMU
+    uses: docker/setup-qemu-action@v2
+    - name: Set up Docker Buildx
+    uses: docker/setup-buildx-action@v2
+    - name: Log in to the Container registry
+    uses: docker/login-action@65b78e6e13532edd9afa3aa52ac7964289d1a9c1
+    with:
+        registry: ghcr.io
+        username: ${{ github.actor }}
+        password: ${{ secrets.CONTAINER_REGISTRY_PAT }}
+    - name: Build and push
+    uses: docker/build-push-action@v3
+    with:
+        context: ./backend
+        file: ./backend/Dockerfile.prod
+        push: true
+        platforms: linux/amd64,linux/arm64
+        tags: ghcr.io/zane-ops/backend:pr-${{ github.event.pull_request.number }},ghcr.io/zane-ops/backend:${{ github.sha }}
+        cache-from: |
+        type=registry,ref=ghcr.io/zane-ops/backend:pr-${{ github.event.pull_request.number }}
+        type=registry,ref=ghcr.io/zane-ops/backend:canary
+        cache-to: type=inline
+uses: actions/hello-world-javascript-action@v1
+with:
+  who-to-greet: 'Mona the Octocat'
+```
--- index.ts
+++ index.ts
@@ -36,9 +36,21 @@ export const env = createEnv({
             .url("Invalid URL for the dashboard base URL"),
         COMMIT_MESSAGE: z.string().optional(),
     },
-    runtimeEnv: Bun.env,
+    runtimeEnv: process.env,
     emptyStringAsUndefined: true,
 });
+
+const inputSchema = z.object({
+    username: z.string().min(1, "The zaneops username is required"),
+    password: z.string().min(1, "The zaneops password is required"),
+    projectSlug: z.string().regex(slugRegex, "Invalid slug"),
+    serviceSlug: z.string().regex(slugRegex, "Invalid slug"),
+    serviceImage: z.string().min(1, "The image of your service is required"),
+    commitMessage: z.string().url("Invalid URL for the dashboard base URL"),
+    zaneDashboardBaseUrl: z.string().optional(),
+});
+
+type Input = z.infer<typeof inputSchema>;
 
 async function parseResponseBody(response: Response) {
     return response.headers.get("content-type") === "application/json"
@@ -47,170 +59,202 @@ async function parseResponseBody(response: Response) {
 }
 
 async function deployScript() {
-    const username = env.ZANE_USERNAME;
-    const password = env.ZANE_PASSWORD;
-    const projectSlug = env.ZANE_PROJECT_SLUG;
-    const serviceSlug = env.ZANE_SERVICE_SLUG;
-    const serviceImage = env.SERVICE_IMAGE;
-    const commitMessage =
-        env.COMMIT_MESSAGE ??
-        `auto-deploy from commit ${process.env.COMMIT_SHA}`;
-    const zaneDashboardBaseUrl = env.ZANE_DASHBOARD_BASE_URL;
-
-    console.log(
-        `Getting the CSRF token on ZaneOps API at ${colors.blue(
-            zaneDashboardBaseUrl
-        )}...`
-    );
-    const csrfResponse = await fetch(`${zaneDashboardBaseUrl}/api/csrf`);
-    const csrfToken = cookie.parseSetCookie(
-        csrfResponse.headers.get("set-cookie") ?? ""
-    ).value;
-
-    if (csrfResponse.status !== 200) {
-        console.log(
-            colors.red("❌ Failed to get CSRF token from ZaneOps API ❌")
-        );
-        console.log(
-            `Received status code from zaneops API : ${colors.red(
-                csrfResponse.status
-            )}`
-        );
-
-        console.log("Received response from zaneops API : ");
-        console.dir(await parseResponseBody(csrfResponse));
-        process.exit(1);
-    } else {
-        console.log(`Got the CSRF token successfully ✅`);
-    }
-
-    console.log(`Authenticating to ZaneOps API...`);
-    const authResponse = await fetch(`${zaneDashboardBaseUrl}/api/auth/login`, {
-        method: "POST",
-        headers: {
-            "x-csrftoken": csrfToken,
-            cookie: `csrftoken=${csrfToken}`,
-            "content-type": "application/json",
-        },
-        body: JSON.stringify({ username, password }),
-    });
-
-    if (authResponse.status !== 201) {
-        console.log(colors.red("❌ Failed to authenticate to ZaneOps API ❌"));
-        console.log(
-            `Received status code from zaneops API : ${colors.red(
-                authResponse.status
-            )}`
-        );
-
-        console.log("Received response from zaneops API : ");
-        console.dir(await parseResponseBody(authResponse));
-        process.exit(1);
-    } else {
-        console.log(`Successfully Authenticated to ZaneOps API ✅`);
-    }
-
-    const sessionIdCookieStr = cookie
-        .splitSetCookieString(authResponse.headers.get("set-cookie") ?? "")
-        .filter((cookieStr) => cookieStr.startsWith("sessionid"))[0];
-
-    const sessionId = cookie.parseSetCookie(sessionIdCookieStr).value;
-
-    const requestCookie = [
-        cookie.serialize("sessionid", sessionId),
-        cookie.serialize("csrftoken", csrfToken),
-        "",
-    ].join(";");
-
-    console.log(
-        `Updating the image for the service ${colors.orange(
-            serviceSlug
-        )} in the project ${colors.orange(projectSlug)}...`
-    );
-    const requestChangeResponse = await fetch(
-        `${zaneDashboardBaseUrl}/api/projects/${projectSlug}/request-service-changes/docker/${serviceSlug}/`,
-        {
-            method: "put",
-            headers: {
-                "x-csrftoken": csrfToken,
-                cookie: requestCookie,
-                "content-type": "application/json",
-            },
-            body: JSON.stringify({
-                type: "UPDATE",
-                new_value: serviceImage,
-                field: "image",
+    try {
+        const input: Record<keyof Input, any> = {
+            username: core.getInput("username", {
+                trimWhitespace: true,
             }),
-        }
-    );
-
-    if (requestChangeResponse.status !== 200) {
-        console.log(
-            colors.red(
-                "❌ Failed to update the image of the service on ZaneOps API ❌"
-            )
-        );
-        console.log(
-            `Received status code from zaneops API : ${colors.red(
-                requestChangeResponse.status
-            )}`
-        );
-
-        console.log("Received response from zaneops API : ");
-        console.dir(await parseResponseBody(requestChangeResponse));
-        process.exit(1);
-    } else {
-        console.log(
-            `Successfully Updated the image to ${colors.orange(
-                serviceImage
-            )} ✅`
-        );
-    }
-
-    console.log(
-        `Queuing a new deployment for the service ${colors.orange(
-            serviceSlug
-        )}...`
-    );
-    const deploymentResponse = await fetch(
-        `${zaneDashboardBaseUrl}/api/projects/${projectSlug}/deploy-service/docker/${serviceSlug}/`,
-        {
-            method: "put",
-            headers: {
-                "x-csrftoken": csrfToken,
-                cookie: requestCookie,
-                "content-type": "application/json",
-            },
-            body: JSON.stringify({
-                commit_message: commitMessage,
+            password: core.getInput("password", {
+                trimWhitespace: true,
             }),
+            projectSlug: core.getInput("project-slug", {
+                trimWhitespace: true,
+            }),
+            serviceSlug: core.getInput("service-slug", {
+                trimWhitespace: true,
+            }),
+            serviceImage: core.getInput("service-image", {
+                trimWhitespace: true,
+            }),
+            commitMessage: core.getInput("commit-message", {
+                trimWhitespace: true,
+            }),
+            zaneDashboardBaseUrl: core.getInput("base-url", {
+                trimWhitespace: true,
+            }),
+        };
+        const {
+            username,
+            password,
+            projectSlug,
+            serviceImage,
+            serviceSlug,
+            zaneDashboardBaseUrl,
+            commitMessage = `auto-deploy from commit ${github.context.payload.comment}`,
+        } = inputSchema.parse(input);
+
+        console.log(
+            `Getting the CSRF token on ZaneOps API at ${colors.blue(
+                zaneDashboardBaseUrl
+            )}...`
+        );
+        const csrfResponse = await fetch(`${zaneDashboardBaseUrl}/api/csrf`);
+        const csrfToken = cookie.parseSetCookie(
+            csrfResponse.headers.get("set-cookie") ?? ""
+        ).value;
+
+        if (csrfResponse.status !== 200) {
+            console.log(
+                colors.red("❌ Failed to get CSRF token from ZaneOps API ❌")
+            );
+            console.log(
+                `Received status code from zaneops API : ${colors.red(
+                    csrfResponse.status
+                )}`
+            );
+
+            console.log("Received response from zaneops API : ");
+            console.dir(await parseResponseBody(csrfResponse));
+            core.setFailed("Failure");
+        } else {
+            console.log(`Got the CSRF token successfully ✅`);
         }
-    );
 
-    if (deploymentResponse.status === 200) {
-        const deployment = await deploymentResponse.json();
-        console.log(`Deployment queued succesfully ✅`);
-        console.log(
-            `inspect here ➡️ ${colors.blue(
-                `${zaneDashboardBaseUrl}/project/${projectSlug}/services/docker/${serviceSlug}/deployments/${deployment.hash}`
-            )}`
-        );
-    } else {
-        console.log(colors.red("❌ Failed to queue deployment ❌"));
-        console.log(
-            `Received status code from zaneops API : ${colors.red(
-                deploymentResponse.status
-            )}`
+        console.log(`Authenticating to ZaneOps API...`);
+        const authResponse = await fetch(
+            `${zaneDashboardBaseUrl}/api/auth/login`,
+            {
+                method: "POST",
+                headers: {
+                    "x-csrftoken": csrfToken,
+                    cookie: `csrftoken=${csrfToken}`,
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify({ username, password }),
+            }
         );
 
-        const response =
-            deploymentResponse.headers.get("content-type") ===
-            "application/json"
-                ? await deploymentResponse.json()
-                : await deploymentResponse.text();
-        console.log("Received response from zaneops API : ");
-        console.dir(response);
-        process.exit(1);
+        if (authResponse.status !== 201) {
+            console.log(
+                colors.red("❌ Failed to authenticate to ZaneOps API ❌")
+            );
+            console.log(
+                `Received status code from zaneops API : ${colors.red(
+                    authResponse.status
+                )}`
+            );
+
+            console.log("Received response from zaneops API : ");
+            console.dir(await parseResponseBody(authResponse));
+            core.setFailed("Failure");
+        } else {
+            console.log(`Successfully Authenticated to ZaneOps API ✅`);
+        }
+
+        const sessionIdCookieStr = cookie
+            .splitSetCookieString(authResponse.headers.get("set-cookie") ?? "")
+            .filter((cookieStr) => cookieStr.startsWith("sessionid"))[0];
+
+        const sessionId = cookie.parseSetCookie(sessionIdCookieStr).value;
+
+        const requestCookie = [
+            cookie.serialize("sessionid", sessionId),
+            cookie.serialize("csrftoken", csrfToken),
+            "",
+        ].join(";");
+
+        console.log(
+            `Updating the image for the service ${colors.orange(
+                serviceSlug
+            )} in the project ${colors.orange(projectSlug)}...`
+        );
+        const requestChangeResponse = await fetch(
+            `${zaneDashboardBaseUrl}/api/projects/${projectSlug}/request-service-changes/docker/${serviceSlug}/`,
+            {
+                method: "put",
+                headers: {
+                    "x-csrftoken": csrfToken,
+                    cookie: requestCookie,
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify({
+                    type: "UPDATE",
+                    new_value: serviceImage,
+                    field: "image",
+                }),
+            }
+        );
+
+        if (requestChangeResponse.status !== 200) {
+            console.log(
+                colors.red(
+                    "❌ Failed to update the image of the service on ZaneOps API ❌"
+                )
+            );
+            console.log(
+                `Received status code from zaneops API : ${colors.red(
+                    requestChangeResponse.status
+                )}`
+            );
+
+            console.log("Received response from zaneops API : ");
+            console.dir(await parseResponseBody(requestChangeResponse));
+            core.setFailed("Failure");
+        } else {
+            console.log(
+                `Successfully Updated the image to ${colors.orange(
+                    serviceImage
+                )} ✅`
+            );
+        }
+
+        console.log(
+            `Queuing a new deployment for the service ${colors.orange(
+                serviceSlug
+            )}...`
+        );
+        const deploymentResponse = await fetch(
+            `${zaneDashboardBaseUrl}/api/projects/${projectSlug}/deploy-service/docker/${serviceSlug}/`,
+            {
+                method: "put",
+                headers: {
+                    "x-csrftoken": csrfToken,
+                    cookie: requestCookie,
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify({
+                    commit_message: commitMessage,
+                }),
+            }
+        );
+
+        if (deploymentResponse.status === 200) {
+            const deployment = await deploymentResponse.json();
+            console.log(`Deployment queued succesfully ✅`);
+            console.log(
+                `inspect here ➡️ ${colors.blue(
+                    `${zaneDashboardBaseUrl}/project/${projectSlug}/services/docker/${serviceSlug}/deployments/${deployment.hash}`
+                )}`
+            );
+        } else {
+            console.log(colors.red("❌ Failed to queue deployment ❌"));
+            console.log(
+                `Received status code from zaneops API : ${colors.red(
+                    deploymentResponse.status
+                )}`
+            );
+
+            const response =
+                deploymentResponse.headers.get("content-type") ===
+                "application/json"
+                    ? await deploymentResponse.json()
+                    : await deploymentResponse.text();
+            console.log("Received response from zaneops API : ");
+            console.dir(response);
+            core.setFailed("Failure");
+        }
+    } catch (error) {
+        core.setFailed((error as any).message);
     }
 }
 
